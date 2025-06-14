aa7bc9e90db6e5440ea53afb58d1ac0eaa36a3fe
Jean Michel Deschamps <jean.deschamps@ailos.coop.br>
2025-06-14 17:40:06 -0300

    Alteração do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header className="bg-gradient-to-r from-green-900 via-green-800 to-green-700 shadow-lg py-6 mb-8">
       <h1 className="text-4xl font-extrabold text-green-200 text-center tracking-wide drop-shadow-lg">
-        Meu Blog
+        8AOJR - FIAP - FRONT END
       </h1>
     </header>
   );
